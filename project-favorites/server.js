29448087f7718dd978f76168ece169378e976cf2
Luan-Reis <luanreis600@gmail.com>
2022-11-14 20:43:56 -0500

    conteúdo final

--- project-favorites/server.js
+++ project-favorites/server.js
@@ -5,16 +5,36 @@ const fs = require('fs')
 http.createServer((req, res)=> {
     // res.end(console.log(req))
 
-    // definindo rotas para a aplicação
-    if(req.url === '/')
-        fs.readFile(
-            path.join(__dirname, 'public', 'index.html'), // dirname= pasta onde estou
-            (err, content) => {
-                if (err) throw err 
-                    res.end(content)
-            }
+    
+    
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+   
+    // pegando a extensão do documento
+    const extname = path.extname(filePath)
 
-        )
+    // criando um vetor com as extensões permitidas
+    const allowedFileTypes = ['.html', '.js', '.css']
+
+
+    const allowed = allowedFileTypes.find(
+        (item) => item === extname
+    )
+    
+    if(!allowed) return
+    
+    
+    // // definindo rotas para a aplicação
+    // if(req.url === '/')
+    fs.readFile(
+        // path.join(__dirname, 'public', 'index.html'), // dirname= pasta onde estou
+        filePath,
+        (err, content) => {
+            if (err) throw err 
+                res.end(content)
+        }
+    )
+    //     )
         
     // res.write('<h1>Olá!</h1>')
     // res.end()
